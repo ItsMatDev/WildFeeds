1ded9908ea5b5a3e3d2097b2bc33b92d970ad454
LucasMerino10 <lucas.merino9@gmail.com>
2023-09-25 21:49:53 +0200

    Started to work on the comment section display

--- javascript.js
+++ javascript.js
@@ -21,18 +21,18 @@ createNewPost.addEventListener("click", (event) => {
     emptyPostContentPicture.alt = ""
     emptyPostShareButton.innerHTML = "Create"
 
-    emptyPostShareButton.addEventListener("click", (event) =>{
-        
-            // Récupérez la valeur de l'input
-            const inputValue = inputElement.value;
-            const inputImageValue = inputImageSource.value
-        
-            // Mettez à jour l'innerHTML de .postContentText avec la valeur de l'input
-            emptyPostContentText.innerHTML = inputValue;
-            emptyPostContentPicture.src = inputImageValue
+    emptyPostShareButton.addEventListener("click", (event) => {
+
+        // Récupérez la valeur de l'input
+        const inputValue = inputElement.value;
+        const inputImageValue = inputImageSource.value
+
+        // Mettez à jour l'innerHTML de .postContentText avec la valeur de l'input
+        emptyPostContentText.innerHTML = inputValue;
+        emptyPostContentPicture.src = inputImageValue
     })
 
-}) 
+})
 
 
 
@@ -48,3 +48,52 @@ buttonShare.addEventListener("click", (event) => {
     document.body.insertBefore(postShared, createNewPost.nextSibling);
 
 })
+
+// Gestion de l'espace commentaires
+
+const buttonComments = document.querySelector('#buttonComments1');
+const buttonBox = document.querySelector('.buttonBox1');
+const post1 = document.querySelector('.post1');
+const comments = [
+    { name: "Name Lastname", picture: "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_640.png", comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit." },
+    { name: "Name Lastname", picture: "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_640.png", comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt autem iusto sequi eius exercitationem, nihil aliquam consequatur repudiandae, nulla cupiditate expedita explicabo praesentium." },
+    { name: "Name Lastname", picture: "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_640.png", comment: "Lorem ipsum3" }
+]
+
+// création de l'espace commentaire du premier post
+const hr = document.createElement('hr');
+hr.classList.add('commentHr');
+post1.appendChild(hr);
+const commentsContainer = document.createElement('div');
+commentsContainer.classList.add('commentsContainer');
+post1.appendChild(commentsContainer);
+
+comments.forEach(comment => {
+    const commentBox = document.createElement('div');
+    commentBox.classList.add('commentBox');
+    commentsContainer.appendChild(commentBox);
+    const commentProfile = document.createElement('div');
+    commentProfile.classList.add('commentProfile');
+    commentBox.appendChild(commentProfile);
+    const profilePicture = document.createElement('img');
+    profilePicture.src = comment.picture;
+    profilePicture.classList.add('commentPicture');
+    commentProfile.appendChild(profilePicture);
+    const profileName = document.createElement('h3');
+    profileName.classList.add('commentName');
+    profileName.innerText = comment.name;
+    commentProfile.appendChild(profileName);
+    const commentText = document.createElement('p');
+    commentText.classList.add('commentText');
+    commentText.innerText = comment.comment;
+    commentBox.appendChild(commentText);
+})
+
+
+// Gestion de l'affichage de la section commentaire
+buttonComments.addEventListener('click', function () {
+    commentsContainer.classList.toggle('visible');
+    hr.classList.toggle('visible');
+})
+
+
