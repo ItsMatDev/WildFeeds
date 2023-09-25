377e39324910639e8224933dbaa54c8087f91754
ItsMatDev <thierry.matthieu@live.fr>
2023-09-25 21:50:19 +0200

    Table task and more about project

--- javascript.js
+++ javascript.js
@@ -1,50 +1,62 @@
-const post = document.querySelector('.post')
-const emptyPost = post.cloneNode(true)
-const emptyPostContentText = emptyPost.querySelector('.postContentText')
-const emptyPostContentPicture = emptyPost.querySelector('.postContentPicture')
-const emptyPostShareButton = emptyPost.querySelector('.buttonShare')
+const createPostButton = document.querySelectorAll('.createPost');
 
-const createNewPost = document.querySelector('.createPost')
+createPostButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        const post = document.querySelector('.post');
+        const emptyPost = post.cloneNode(true);
 
-createNewPost.addEventListener("click", (event) => {
-    document.body.insertBefore(emptyPost, createNewPost.nextSibling)
+        const inputElement = document.createElement('input');
+        const inputImageSource = document.createElement('input');
+        inputElement.type = 'text';
+        emptyPost.querySelector('.postContentText').innerHTML = "";
+        emptyPost.querySelector('.postContentText').appendChild(inputElement);
+        inputElement.placeholder = "Write your text here";
+        emptyPost.querySelector('.postContentText').appendChild(inputImageSource);
+        inputImageSource.placeholder = "Put your image URL here";
+        emptyPost.querySelector('.postContentPicture').src = "";
+        emptyPost.querySelector('.postContentPicture').alt = ""
+        emptyPost.querySelector('.buttonShare').innerHTML = "Create";
 
-    const inputElement = document.createElement('input');
-    const inputImageSource = document.createElement('input')
-    inputElement.type = 'text'
-    emptyPostContentText.innerHTML = ""
-    emptyPostContentText.appendChild(inputElement)
-    inputElement.placeholder = "Write your text here"
-    emptyPostContentText.appendChild(inputImageSource)
-    inputImageSource.placeholder = "Put your url image here"
-    emptyPostContentPicture.src = ""
-    emptyPostContentPicture.alt = ""
-    emptyPostShareButton.innerHTML = "Create"
+        const createPostButton = button;
+        document.body.insertBefore(emptyPost, createPostButton.nextSibling);
 
-    emptyPostShareButton.addEventListener("click", (event) =>{
-        
-            // Récupérez la valeur de l'input
+        emptyPost.querySelector('.buttonShare').addEventListener("click", (event) => {
             const inputValue = inputElement.value;
-            const inputImageValue = inputImageSource.value
+            const inputImageValue = inputImageSource.value;
+            const postContentText = emptyPost.querySelector('.postContentText');
+            const postContentPicture = emptyPost.querySelector('.postContentPicture');
+
+            postContentText.innerHTML = inputValue;
+            postContentPicture.src = inputImageValue;
+        });
         
-            // Mettez à jour l'innerHTML de .postContentText avec la valeur de l'input
-            emptyPostContentText.innerHTML = inputValue;
-            emptyPostContentPicture.src = inputImageValue
-    })
+        const buttonSetting = emptyPost.querySelector('.buttonSettingsPicture');
+        buttonSetting.addEventListener('click', (event) => {
+            const parentPost = event.currentTarget.closest('.post');
+            if (parentPost) {
+                document.body.removeChild(parentPost);
+                console.log("oui");
+            }
+        });
+    });
+});
 
-}) 
-
-
-
-
-
-
-const buttonShare = document.querySelector('.buttonShare3')
-
+const buttonShare = document.querySelector('.buttonShare3');
 buttonShare.addEventListener("click", (event) => {
-
     const post = document.querySelector('.post3');
     const postShared = post.cloneNode(true);
-    document.body.insertBefore(postShared, createNewPost.nextSibling);
+    document.body.insertBefore(postShared, createPostButton.nextSibling);
+});
 
-})
+
+const buttonSettings = document.querySelectorAll('.buttonSettingsPicture');
+
+buttonSettings.forEach((buttonSetting) => {
+    buttonSetting.addEventListener('click', (event) => {
+        const parentPost = event.currentTarget.closest('.post');
+        if (parentPost) {
+            document.body.removeChild(parentPost);
+            console.log("oui");
+        }
+    });
+});
