6a7f89b23fdfdd098911c0089e3be0b28d99942a
ItsMatDev <thierry.matthieu@live.fr>
2023-09-26 16:10:06 +0200

    Fixing share button and adding in into a function, now even newPost can be share

--- javascript.js
+++ javascript.js
@@ -1,80 +1,84 @@
 const createPostButton = document.querySelectorAll('.createPost');
-
-createPostButton.forEach((button) => {
-    button.addEventListener('click', () => {
-        const post = document.querySelector('.post');
-        const emptyPost = post.cloneNode(true);
-
-        const inputElement = document.createElement('input');
-        const inputImageSource = document.createElement('input');
-        inputElement.type = 'text';
-        emptyPost.querySelector('.postContentText').innerHTML = "";
-        emptyPost.querySelector('.postContentText').appendChild(inputElement);
-        inputElement.placeholder = "Write your text here";
-        emptyPost.querySelector('.postContentText').appendChild(inputImageSource);
-        inputImageSource.placeholder = "Put your image URL here";
-        emptyPost.querySelector('.postContentPicture').src = "";
-        emptyPost.querySelector('.postContentPicture').alt = ""
-        emptyPost.querySelector('.buttonShare').innerHTML = "Create";
-
-        const createPostButton = button;
-        document.body.insertBefore(emptyPost, createPostButton.nextSibling);
-
-        emptyPost.querySelector('.buttonShare').addEventListener("click", (event) => {
-            const inputValue = inputElement.value;
-            const inputImageValue = inputImageSource.value;
-            const postContentText = emptyPost.querySelector('.postContentText');
-            const postContentPicture = emptyPost.querySelector('.postContentPicture');
-
-            postContentText.innerHTML = inputValue;
-            postContentPicture.src = inputImageValue;
-        });
-        
-        const buttonSetting = emptyPost.querySelector('.buttonSettingsPicture');
-        buttonSetting.addEventListener('click', (event) => {
-            const parentPost = event.currentTarget.closest('.post');
-            if (parentPost) {
-                document.body.removeChild(parentPost);
-                console.log("oui");
-            }
-        });
-    });
-});
-
-    const createPost = document.querySelector('.createPost')
-
-    const shareButtons = document.querySelectorAll('.buttonShare');
-
-    shareButtons.forEach((shareButton) => {
-        shareButton.addEventListener('click', (event) => {
-            const parentArticle = event.currentTarget.closest('.post');
-
-            if (parentArticle) {
-                const clonedArticle = parentArticle.cloneNode(true);
-                document.body.insertBefore(clonedArticle, createPost.nextSibling);
-            }
-
-            const buttonSettings = document.querySelectorAll('.buttonSettingsPicture');
-
-            buttonSettings.forEach((buttonSetting) => {
-                buttonSetting.addEventListener('click', (event) => {
-                    const parentPost = event.currentTarget.closest('.post');
-                    if (parentPost) {
-                        document.body.removeChild(parentPost);
-                    }
-                });
-            });
-        });
-});
-
-
 const buttonSettings = document.querySelectorAll('.buttonSettingsPicture');
+const buttonShare = document.querySelectorAll('.buttonShare');
 
-buttonSettings.forEach((buttonSetting) => {
-    buttonSetting.addEventListener('click', (event) => {
-        const parentPost = event.currentTarget.closest('.post');
-        if (parentPost) {
-            document.body.removeChild(parentPost);
-        }
+function setButtonSettings (button) {
+
+    button.forEach((button) => {
+        button.addEventListener('click', () => {
+            const parentPost = button.closest('.post');
+                document.body.removeChild(parentPost);
+        });
     });
+    }
+
+    function setButtonShare(button) {
+        button.forEach((button) => {
+            button.addEventListener('click', () => {
+                const parentPost = button.closest('.post');
+                
+                    const sharedPost = parentPost.cloneNode(true);
+                    document.body.insertBefore(sharedPost, createPostButton[0].nextSibling);
+                    const buttonSetting = sharedPost.querySelectorAll('.buttonSettingsPicture');
+                    setButtonSettings(buttonSetting);
+
+    
+                
+            });
+    });
+    }
+
+setButtonSettings (buttonSettings);
+setButtonShare(buttonShare);
+
+
+createPostButton.forEach((createPostButton) => {
+    createPostButton.addEventListener('click', function () {
+
+            const post = document.querySelector('.post');
+            const emptyPost = post.cloneNode(true);
+            const inputElement = document.createElement('input');
+            const inputImageSource = document.createElement('input');
+            const buttonSettingNewPost = emptyPost.querySelectorAll('.buttonSettingsPicture');
+            const buttonShareNewPost = emptyPost.querySelectorAll('.buttonShare')
+
+            inputElement.type = 'text';
+            emptyPost.querySelector('.postContentText').innerHTML = "";
+            emptyPost.querySelector('.postContentText').appendChild(inputElement);
+            inputElement.placeholder = "Write your text here";
+            emptyPost.querySelector('.postContentText').appendChild(inputImageSource);
+            inputImageSource.placeholder = "Put your image URL here";
+            emptyPost.querySelector('.postContentPicture').src = "";
+            emptyPost.querySelector('.postContentPicture').alt = "";  
+            const buttonShare = emptyPost.querySelector('.buttonShare');
+            const buttonCreate = buttonShare.cloneNode(true);
+            const buttonBox = emptyPost.querySelector('.buttonBox');
+            buttonBox.appendChild(buttonCreate);
+            buttonCreate.innerHTML = "Create"; 
+            buttonCreate.classList.remove("buttonShare")
+            buttonCreate.classList.add("buttonCreate")
+
+
+            
+
+    
+            buttonCreate.addEventListener("click", () => {
+                const inputValue = inputElement.value;
+                const inputImageValue = inputImageSource.value;
+                const postContentText = emptyPost.querySelector('.postContentText');
+                const postContentPicture = emptyPost.querySelector('.postContentPicture');
+    
+                postContentText.innerHTML = inputValue;
+                postContentPicture.src = inputImageValue;
+                buttonCreate.style.display = 'none';
+            });
+
+            document.body.insertBefore(emptyPost, createPostButton.nextSibling);
+
+            setButtonSettings(buttonSettingNewPost);
+            setButtonShare(buttonShareNewPost);
+        });
 });
+
+ 
+    
