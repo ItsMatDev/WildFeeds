dc3294be9cd7dcfc7eb7b4a7de1d8ece83ef264a
LucasMerino10 <lucas.merino9@gmail.com>
2023-09-27 11:42:35 +0200

    Added new comment feature

--- javascript.js
+++ javascript.js
@@ -104,7 +104,6 @@ createPostButton.forEach((createPostButton) => {
 // Gestion de l'espace commentaires
 
 const buttonComments = document.querySelectorAll('.buttonComments');
-const buttonSubmit = document.querySelectorAll('.buttonSubmit');
 const post1 = document.querySelector('.post1');
 const commentsPost1 = [
     { name: "Name Lastname", picture: "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_640.png", comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit." },
@@ -147,12 +146,12 @@ posts.forEach((post, index) => {
     const input = document.createElement('div');
     input.classList.add('inputDiv');
     commentsContainer.appendChild(input);
-    const commentInput = document.createElement('input');
+    const commentInput = document.createElement('textarea');
     commentInput.classList.add('commentInput');
     commentInput.placeholder = "write a new comment";
     input.appendChild(commentInput);
     const submitButton = document.createElement('button');
-    submitButton.classList.add('buttonSubmit');
+    submitButton.classList.add('buttonSubmit', 'basicStyleButton');
     submitButton.innerText = "Submit";
     input.appendChild(submitButton);
     // Selectionne les commentaires à afficher en fonction de l'index du post
@@ -187,8 +186,8 @@ posts.forEach((post, index) => {
 buttonComments.forEach(button => {
     button.addEventListener('click', function () {
         const currentPost = button.closest('.post');
-        const commentContainer = currentPost.lastChild;
-        const hr = commentContainer.previousSibling;
+        const commentContainer = currentPost.querySelector('.commentsContainer');
+        const hr = currentPost.querySelector('hr');
         visible(hr, commentContainer);
     });
 })
@@ -205,34 +204,44 @@ function commentsSelection(index) {
     }
 }
 
+// Création d'un nouveau commentaire
+const buttonSubmit = document.querySelectorAll('.buttonSubmit');
+const currentUser = [
+    { username: "Current User", profilePicture: "https://cdn.pixabay.com/photo/2014/04/02/10/25/man-303792_640.png" }
+]
 
+// Evênement clic sur le bouton submit, créée un nouveau commentaire
 buttonSubmit.forEach(button => {
     button.addEventListener('click', function () {
-        const commentContainer = button.closest('.commentContainer');
+        const commentsContainer = button.closest('.commentsContainer');
+        const comment = commentsContainer.querySelector('textarea').value;
+        button.previousSibling.value = "";
+        firstComment = commentsContainer.querySelector('.commentBox');
+        createComment(commentsContainer, comment, firstComment);
     })
 })
 
-
-// Ajouter un nouveau commentaire saisi par l'utilisateur
-const commentBox = document.createElement('div');
-commentBox.classList.add('commentBox');
-commentsContainer.appendChild(commentBox);
-const commentProfile = document.createElement('div');
-commentProfile.classList.add('commentProfile');
-commentBox.appendChild(commentProfile);
-const profilePicture = document.createElement('img');
-profilePicture.src = comment.picture;
-profilePicture.classList.add('commentPicture');
-commentProfile.appendChild(profilePicture);
-const commentBody = document.createElement('div');
-commentBody.classList.add('commentBody');
-commentBox.appendChild(commentBody);
-const profileName = document.createElement('h3');
-profileName.classList.add('commentName');
-profileName.innerText = comment.name;
-commentBody.appendChild(profileName);
-const commentText = document.createElement('p');
-commentText.classList.add('commentText');
-commentText.innerText = comment.comment;
-commentBody.appendChild(commentText);
-
+// Fonction permettant la création des éléments composant le nouveau commentaire
+function createComment(commentsContainer, comment, firstComment) {
+    const commentBox = document.createElement('div');
+    commentBox.classList.add('commentBox');
+    commentsContainer.insertBefore(commentBox, firstComment);
+    const commentProfile = document.createElement('div');
+    commentProfile.classList.add('commentProfile');
+    commentBox.appendChild(commentProfile);
+    const profilePicture = document.createElement('img');
+    profilePicture.src = currentUser[0].profilePicture;
+    profilePicture.classList.add('commentPicture');
+    commentProfile.appendChild(profilePicture);
+    const commentBody = document.createElement('div');
+    commentBody.classList.add('commentBody');
+    commentBox.appendChild(commentBody);
+    const profileName = document.createElement('h3');
+    profileName.classList.add('commentName');
+    profileName.innerText = currentUser[0].username;
+    commentBody.appendChild(profileName);
+    const commentText = document.createElement('p');
+    commentText.classList.add('commentText');
+    commentText.innerText = comment;
+    commentBody.appendChild(commentText);
+}
